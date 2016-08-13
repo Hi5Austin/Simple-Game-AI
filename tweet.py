40cc3c1215a0734c4d2055a40117da7731008766
Austin Carvey <austin@carvey.me>
2016-08-13 11:29:49 -0400

    delete

--- tweet.py
+++
@@ -1,2 +0,0 @@
-from TwitterAPI import TwitterAPI
-api = TwitterAPI('HaRkHchUXROSHDqvBbynCsyDc','vs7vn4JFpV4VIq90LcwRaepB8yJQWitxEGyTzrn8HsNNxns962','763153901323419649-gNJRtqOH7aSQ0bqqCaPkDC1ZoeJqqid','Xe5eGwZ9buxvDBBKJbwGKuQC3ADsw21bg5QKAQBRyxuVL')
